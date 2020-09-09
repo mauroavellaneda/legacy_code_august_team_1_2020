5f3b5ed7115e8fb7c0740c55530b056afec83a5d
Mauro Avellaneda <mauroeavellaneda@gmail.com>
2020-09-09 13:04:11 +0200

    cleanning the three to pull from master

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 require 'coveralls'
-Coveralls.wear!('rails')
+Coveralls.wear_merged!('rails')
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../config/environment', __dir__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
